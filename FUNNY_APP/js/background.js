f04854630663f4adc8ee41369fe9f990a701cf7a
Chaenii <83631140+Chaenii@users.noreply.github.com>
2022-06-20 01:00:33 +0900

    FEAT: 배경이미지 경로 수정

--- FUNNY_APP/js/background.js
+++ FUNNY_APP/js/background.js
@@ -2,7 +2,7 @@ const img =["0.jpeg","1.jpeg","2.jpeg","3.jpeg"];
 const choseImg = img[Math.floor(Math.random()*img.length)];
 
 const body = document.querySelector("body");
-body.style.backgroundImage=`url(img/${choseImg})`;
+body.style.backgroundImage=`url(./img/${choseImg})`;
 // const bgImg = document.createElement("img");
 // bgImg.src=`img/${choseImg}`;
 // document.body.appendChild(bgImg);
